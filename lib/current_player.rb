db3f356a798591b9045a165e554904c6a9266f1e
Andrew C <acchang4@gmail.com>
2020-04-10 15:29:06 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,18 @@
+def turn_count(turn)
+  #counter = 1
+  move_count = []
+  turn.each do |unit|
+    #counter = 0
+    if unit == "X" || unit == "O"
+      #puts "spot taken"
+      move_count.push(1)
+    #else
+      #puts "spot free"
+      #counter += 1
+    end
+  end
+  move_count.length.to_i
+end
 
 def current_player(board)
   #if turn_count(board) %  == 0
