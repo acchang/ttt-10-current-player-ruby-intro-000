d487fb7e7d9c736efbac29ce2a070643729c3b2c
Andrew C <acchang4@gmail.com>
2020-04-10 16:03:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,6 @@ def turn_count(turn)
   counter = 0
   turn.each do |unit|
       if unit == "X" || unit == "O"
-      move_count.push(1)
+      counter += 1
     end
   end
-  move_count.length.to_i
-end
